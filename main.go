9dbc721dc6f7afdd788bb0de2ea882c5496c77e0
AAleka <mr.allnur.55@users.noreply.github.com>
2021-01-25 22:52:44 +0600

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Hello, Alnur")
+
+	var temp int
+	temp = 11
+	var variable = 10
+	fmt.Println(temp + variable)
+
+	var name, age = "Alnur", 21
+	fmt.Println(name, age)
+
+	var input string
+	fmt.Scanln(&input)
+	fmt.Println(input)
+
+	hey("Alnur")
+	fmt.Println(heyheyhey("Alnur"))
+}
+
+func hey(name string) {
+	fmt.Println("Hey, " + name)
+}
+
+func heyheyhey(name string) string {
+	return "heyheyhey, " + name
 }
